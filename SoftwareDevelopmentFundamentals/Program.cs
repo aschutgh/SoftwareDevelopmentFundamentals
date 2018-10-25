871f8b2dfcaa24263c6eb9de2933bb73f3c393c2
Arthur S <aschutgh@gmail.com>
2018-10-25 13:48:03 +0200

    Add project files.

---
+++ SoftwareDevelopmentFundamentals/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoftwareDevelopmentFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
